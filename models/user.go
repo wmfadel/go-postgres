d7768c2f57356b427b32dfb3a5f7e04974cccb06
wmfadel <fadel24997@gmail.com>
2023-12-29 18:01:24 +0200

    updated users table to include password field

---
+++ models/user.go
@@ -0,0 +1,27 @@
+package models
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password,omitempty"`
+	Location string `json:"location"`
+	Age      int64  `json:"age"`
+}
+
+func (user *User) HashPassword() error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+func (user *User) CheckPassword(providedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
+	if err != nil {
+		return err
+	}
+	return nil
+}

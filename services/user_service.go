32d9aea5636b950e60763057affe8f53939c5ff4
wmfadel <fadel24997@gmail.com>
2024-01-07 17:00:39 +0200

    migrated to gorm

--- services/user_service.go
+++ services/user_service.go
@@ -1,126 +1,89 @@
 package UserService
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	"go-postgres/database"
 	"go-postgres/models"
 	"log"
+
+	"gorm.io/gorm"
 )
 
-func CreateUser(user *models.User) int64 {
-	user.HashPassword()
-	sqlStatement := `INSERT INTO users (name, password, location, age, email) VALUES ($1, $2, $3, $4, $5) RETURNING userid`
-	var userid int64
-	err := database.Instance.QueryRow(sqlStatement, user.Name, user.Password, user.Location, user.Age, user.Email).Scan(&userid)
-	if err != nil {
-		log.Fatalf("Failed to create user.  %v", err)
+func CreateUser(user *models.User) uint {
+	tx := database.Instance.Create(user)
+	if tx.Error != nil {
+		log.Fatalf("Failed to create user.  %v", tx.Error)
 	}
 	fmt.Println("Created new user")
-	return userid
+	return user.ID
 }
 
-func UpdateUser(id int64, user models.User) int64 {
-	user.HashPassword()
-	sqlStatement := `UPDATE users SET name=$2, password=$3, location=$4, age=$5, email-$6 WHERE userid=$1`
-	res, err := database.Instance.Exec(sqlStatement, id, user.Name, user.Password, user.Location, user.Age, user.Email)
-
-	if err != nil {
-		log.Fatalf("Unable to execute the query. %v", err)
+func UpdateUser(id int64, user models.User) models.User {
+	var updatedUser models.User
+	tx := database.Instance.Model(&updatedUser).Where("id = ?", id).Updates(&user)
+	if tx.Error != nil {
+		log.Fatalf("Unable to execute the query. %v", tx.Error)
 	}
 
-	rowsAffected, err := res.RowsAffected()
-
-	if err != nil {
-		log.Fatalf("Error while checking the affected rows. %v", err)
+	if tx.RowsAffected == 0 {
+		log.Fatalf("Error while checking the affected rows 0")
 	}
 
-	fmt.Printf("Total rows/record affected %v", rowsAffected)
-
-	return rowsAffected
+	return updatedUser
 }
 
 func GetAllUsers() ([]models.User, error) {
-
-	rows, err := database.Instance.Query(`SELECT userid, name, age, location, email FROM users`)
-	if err != nil {
-		log.Fatal("Failed to query users")
-	}
-	defer rows.Close()
 	var users []models.User
-
-	for rows.Next() {
-		var user models.User
-
-		err = rows.Scan(&user.ID, &user.Name, &user.Age, &user.Location, &user.Email)
-		if err != nil {
-			log.Fatalf("Unable to scan the row. %v", err)
-		}
-		users = append(users, user)
+	tx := database.Instance.Find(&users)
+	if tx.Error != nil {
+		log.Fatal("Failed to query users")
+		return nil, tx.Error
 	}
 
-	return users, err
+	return users, tx.Error
 }
 
-func GetUser(Id int64) (models.User, error) {
-
-	row := database.Instance.QueryRow(`SELECT userid, name, age,location, email  FROM users WHERE userid = $1`, Id)
-
+func GetUser(Id uint) (models.User, error) {
 	var user models.User
-	err := row.Scan(&user.ID, &user.Name, &user.Age, &user.Location, &user.Email)
+	tx := database.Instance.First(&user, Id)
 
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned!")
-		return user, errors.New("no rows found")
-	case nil:
-		return user, nil
-	default:
-		log.Fatalf("Unable to scan the row. %v", err)
+	if tx.Error != nil {
+		return user, tx.Error
+	}
+	if errors.Is(tx.Error, gorm.ErrRecordNotFound) {
+		return user, errors.New("user not found")
 	}
 
-	// return empty user on error
-	return user, err
+	return user, nil
 }
 
 func GetUserByEmail(email string) (models.User, error) {
 
-	row := database.Instance.QueryRow(`SELECT * FROM users WHERE email = $1`, email)
-
 	var user models.User
-	err := row.Scan(&user.ID, &user.Name, &user.Age, &user.Location, &user.Password, &user.Email)
+	tx := database.Instance.Where("email = ?", email).First(&user)
 
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned!")
-		return user, errors.New("no rows found")
-	case nil:
-		return user, nil
-	default:
-		log.Fatalf("Unable to scan the row. %v", err)
+	if tx.Error != nil {
+		return user, tx.Error
+	}
+	if errors.Is(tx.Error, gorm.ErrRecordNotFound) {
+		return user, errors.New("user not found")
 	}
 
-	// return empty user on error
-	return user, err
+	return user, nil
 }
 
-func DeleteUser(Id int64) (int64, error) {
+func DeleteUser(Id int64) (models.User, error) {
+	var user models.User
+	tx := database.Instance.Delete(&user, Id)
 
-	res, err := database.Instance.Exec(`DELETE FROM users WHERE userid=$1`, Id)
-
-	if err != nil {
-		return 0, err
+	if tx.Error != nil {
+		return user, tx.Error
 	}
 
-	rowsCount, err := res.RowsAffected()
-	if err != nil {
-		return 0, err
+	if tx.RowsAffected != 1 {
+		return user, errors.New("didn't find the user to delete")
 	}
 
-	if rowsCount != 1 {
-		return 0, errors.New("failed to delete user")
-	}
-
-	return Id, nil
+	return user, nil
 }
